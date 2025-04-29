3c21229e4ca9e872d903bf597dd7e5f957819be3
Elias-Larsson <Elias-Larsson@chasacademy.se>
2025-04-29 15:07:47 +0200

    commit 0

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  template: `
+    <p>
+      button works!
+    </p>
+  `,
+})
+export class ButtonComponent {
+
+}
