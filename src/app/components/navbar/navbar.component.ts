06e9cf4aaac2b32e14e8341a37bdd811e3406d83
Elias-Larsson <Elias-Larsson@chasacademy.se>
2025-05-07 14:35:25 +0200

    refactor: logo change and size change

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ButtonComponent, RouterLink],
   template: `
-      <nav class="flex m-0 flex-row justify-between bg-background p-2 h-16 w-screen">
-
-   <img
-        src="/lootboxlogo.svg"
-        alt="lootbox logo">
-      <img src="/iconamoon_profile-fill.svg" alt="profile icon">
-      </nav>
+    <nav class="flex m-0 flex-row items-center justify-between bg-background py-2 px-2 w-full">
+      <a routerLink="/">
+      <div class="flex flex-row text-white items-center p-2">
+        <img src="/lootbox logo1.svg" alt="lootbox logo">
+        <h1 class="text-3xl pl-6 font-semibold">LOOTBOX KILLA</h1>
+      </div>
+      </a>
+      <a
+      [routerLink]="link()"
+      class="inline-block text-center no-underline bg-primary rounded-xl text-xl py-3 px-5 font-semibold"
+      >{{ button_name() }}</a>
+      <!-- <img src="/iconamoon_profile-fill.svg" alt="profile icon"> -->
+    </nav>
   `,
   styles: ``
 })
 export class NavbarComponent {
-
+  link = input.required<string>();
+  
+  button_name = input.required<string>();
 }
