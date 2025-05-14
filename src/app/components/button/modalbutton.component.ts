fbd2e7f5f68b6cff5762ed697afefa6a06acb5e1
Elias-Larsson <aroezi05@gmail.com>
2025-05-14 17:57:44 +0200

    feat: add deleteProfile function

--- src/app/components/button/modalbutton.component.ts
+++ src/app/components/button/modalbutton.component.ts
@@ -10,23 +10,27 @@ import { Router } from '@angular/router';
       (click)="toggleModal()"
       class="inline-block text-center no-underline bg-primary rounded-2xl py-4 px-12 text-2xl"
     >
-        {{ button_name() }}
+      {{ button_name() }}
     </button>
     @if (showModal) {
-        <div
+    <div
       class="z-10 fixed w-full h-full top-0 left-0 items-center justify-center bg-background/80"
     >
       <div
         class="bg-white flex flex-col justify-center items-center w-96 h-96 m-32 rounded-2xl"
       >
         <button
-            (click)="closeModal()"
+          (click)="closeModal()"
           type="button"
           class="bg-background-light text-white rounded-lg p-1"
         >
           Close
         </button>
-        <form [formGroup]="updateForm" (ngSubmit)="onSubmit()" class="flex flex-col justify-center items-center">
+        <form
+          [formGroup]="updateForm"
+          (ngSubmit)="onSubmitUpdate()"
+          class="flex flex-col justify-center items-center"
+        >
           <h1 class="text-2xl">Edit profile</h1>
           <input
             class="bg-accent-light m-4 px-8 py-2 rounded"
@@ -34,21 +38,21 @@ import { Router } from '@angular/router';
             formControlName="name"
             placeholder="Name"
             required
-          >
+          />
           <input
             class="bg-accent-light m-4 px-8 py-2 rounded"
             type="email"
             formControlName="email"
             placeholder="Email"
             required
-          >
+          />
           <input
             class="bg-accent-light m-4 px-8 py-2 rounded"
             type="password"
             formControlName="password"
             placeholder="Password"
             required
-          >
+          />
           <div class="flex flex-col">
             <button
               class="inline-block text-center no-underline bg-primary rounded-2xl py-2 m-1 w-full"
@@ -59,7 +63,8 @@ import { Router } from '@angular/router';
             <button
               class="inline-block text-center no-underline bg-red-500 rounded-2xl py-1 m-1 w-full"
               type="button"
-              >
+              (click)="deleteProfile()"
+            >
               Delete profile
             </button>
           </div>
@@ -70,48 +75,70 @@ import { Router } from '@angular/router';
   `,
 })
 export class ModalButtonComponent {
-    constructor(private UserService: UserService, private Router: Router) {}
-    showModal = false;
-    button_name = input.required<string>();
-    updateForm: FormGroup = new FormGroup({
-        email: new FormControl(''),
-        name: new FormControl(''),
-        password: new FormControl(''),
-    });
+  constructor(private UserService: UserService, private Router: Router) {}
+  showModal = false;
+  button_name = input.required<string>();
 
-    toggleModal() {
-        this.showModal = !this.showModal;
+  updateForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    name: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  onSubmitUpdate() {
+    const credentials = this.updateForm.value;
+    const accessToken = localStorage.getItem('access_token');
+
+    if (!accessToken) {
+      console.error('No access token found in localStorage');
+      return;
     }
 
-    closeModal() {
-        this.showModal = false;
-    }
+    console.log('Credentials:', credentials);
 
-    onSubmit() {
-        const credentials = this.updateForm.value;
-        const accessToken = localStorage.getItem('access_token'); 
-
-        if (!accessToken) {
-            console.error('No access token found in localStorage');
-            return;
+    this.UserService.updateProfile(credentials, accessToken).subscribe({
+      next: (response) => {
+        if (response) {
+          console.log('Profile updated successfully:', response);
+          this.Router.navigate(['/']);
+        } else {
+          console.error('Update failed: No user data received');
         }
+      },
+      error: (err) => {
+        console.error('Update failed:', err);
+      },
+    });
+  }
 
-        console.log('Access Token:', accessToken);
-        console.log('Credentials:', credentials);
+  deleteProfile() {
+    const accessToken = localStorage.getItem('access_token');
 
-        this.UserService.updateProfile(credentials, accessToken).subscribe({
-            next: (response) => {
-                if (response) {
-                    console.log('Profile updated successfully:', response);
-                    this.Router.navigate(['/']);
-                } else {
-                    console.error('Update failed: No user data received');
-                }
-            },
-            error: (err) => {
-                console.error('Update failed:', err);
-            },
-        });
+    if (!accessToken) {
+      console.error('No access token found in localStorage');
+      return;
     }
-  
+
+    this.UserService.deleteProfile(accessToken).subscribe({
+      next: (response) => {
+        if (response) {
+          console.log('Profile deleted successfully:', response);
+          // this.Router.navigate(['/']);
+        } else {
+          console.error('Delete failed: No user data received');
+        }
+      },
+      error: (err) => {
+        console.error('Delete failed:', err);
+      },
+    });
+  }
 }
