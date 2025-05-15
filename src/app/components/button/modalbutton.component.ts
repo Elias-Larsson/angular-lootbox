572bb59b1755c021cff1ab83c158141419a56555
Elias-Larsson <aroezi05@gmail.com>
2025-05-15 18:05:06 +0200

    refactor: change modal colors

--- src/app/components/button/modalbutton.component.ts
+++ src/app/components/button/modalbutton.component.ts
@@ -17,12 +17,12 @@ import { Router } from '@angular/router';
       class="z-10 fixed w-full h-full top-0 left-0 items-center justify-center bg-background/80"
     >
       <div
-        class="bg-white flex flex-col justify-center items-center w-96 h-96 m-32 rounded-2xl"
+        class="bg-background flex flex-col justify-center items-center w-96 h-96 m-32 rounded-2xl"
       >
         <button
           (click)="closeModal()"
           type="button"
-          class="bg-background-light text-white rounded-lg p-1"
+          class="bg-white text-black rounded-lg p-1"
         >
           Close
         </button>
@@ -33,21 +33,21 @@ import { Router } from '@angular/router';
         >
           <h1 class="text-2xl">Edit profile</h1>
           <input
-            class="bg-accent-light m-4 px-8 py-2 rounded"
+            class="bg-white text-black m-4 px-8 py-2 rounded"
             type="text"
             formControlName="name"
             placeholder="Name"
             required
           />
           <input
-            class="bg-accent-light m-4 px-8 py-2 rounded"
+            class="bg-white text-black m-4 px-8 py-2 rounded"
             type="email"
             formControlName="email"
             placeholder="Email"
             required
           />
           <input
-            class="bg-accent-light m-4 px-8 py-2 rounded"
+            class="bg-white text-black m-4 px-8 py-2 rounded"
             type="password"
             formControlName="password"
             placeholder="Password"
