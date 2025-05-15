13e898f24bad8ef74a4f0a23fd3f9234b17e328a
Elias-Larsson <aroezi05@gmail.com>
2025-05-15 13:45:42 +0200

    feat: add navbar component to register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, NavbarComponent],
   template: `
-  <div class="flex items-center flex-col ">
-    <nav class="flex flex-row justify-between bg-background p-2 h-16 w-screen"></nav>
+  <div class="flex items-center flex-col bg-background">
+
+  <app-navbar link="/login" button_name="Login" class="w-full"></app-navbar>
     <div
-      class="bg-white flex flex-col justify-center items-center w-full max-w-156 h-96 my-32 rounded-2xl"
+      class= "flex flex-col justify-center items-center w-full max-w-156 h-96 my-32 rounded-2xl"
     >
-      <h1 class="text-2xl">Register</h1>
+      <h1 class="text-2xl text-white">Register</h1>
       <form
         id="formRegister"
         [formGroup]="registerForm"
@@ -20,7 +22,7 @@ import { UserService } from '../../services/user.service';
         class="flex flex-col justify-center items-center"
       >
         <input
-          class="bg-accent-light m-4 px-8 py-2 rounded"
+          class="bg-white m-4 px-8 py-2 rounded"
           type="text"
           id="name"
           formControlName="name"
@@ -28,7 +30,7 @@ import { UserService } from '../../services/user.service';
           required
         >
         <input
-          class="bg-accent-light m-4 px-8 py-2 rounded"
+          class="bg-white m-4 px-8 py-2 rounded"
           type="email"
           id="email"
           formControlName="email"
@@ -36,7 +38,7 @@ import { UserService } from '../../services/user.service';
           required
         >
         <input
-          class="bg-accent-light m-4 px-8 py-2 rounded"
+          class="bg-white m-4 px-8 py-2 rounded"
           type="password"
           id="password"
           formControlName="password"
@@ -44,12 +46,14 @@ import { UserService } from '../../services/user.service';
           required
         >
         <button
-          class="inline-block text-center no-underline bg-primary rounded-2xl py-2 px-8 text-xl"
+          class="inline-block text-center no-underline bg-primary rounded-2xl py-2 px-8 text-xl cursor-pointer"
           type="submit"
         >
           Create account
         </button>
-        <p>Already have an account? <a href="/login.html">login</a></p>
+        <p class="text-white">
+          Already have an account? <a routerLink="/login" class="text-blue-300">Login</a>
+        </p>
       </form>
     </div>
 
