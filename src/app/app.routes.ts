443b8435d0e166b0aa660a23c5d17be698476395
Elias-Larsson <aroezi05@gmail.com>
2025-05-15 12:04:48 +0200

    feat: add userbyid route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LandingpageComponent } from './pages/landingpage/landingpage.component'
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { UserByIdComponent } from './pages/profile/userprofile.component';
 
 export const routes: Routes = [
 {path: '', component: LandingpageComponent},
 {path: 'login', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
 {path: 'profile', component: ProfileComponent},
+{path: 'profile/:_id', component: UserByIdComponent},
 ];
 
 
